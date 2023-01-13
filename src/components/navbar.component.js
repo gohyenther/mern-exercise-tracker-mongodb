e15da1a74c4a46552eb639fe5f7aea0b36c5ec80
unknown <goh.yenther@gmail.com>
2023-01-13 15:13:04 +0800

    navbar

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -5,22 +5,33 @@ export default class Navbar extends Component {
 
   render() {
     return (
+    <>
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        <Link to="/" className="navbar-brand">Health Tracker</Link>
         <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/" className="nav-link">Exercises</Link>
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/user" className="nav-link">Create User</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/createExercise" className="nav-link">Create Exercise Log</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/createMeal" className="nav-link">Create Meal Log</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <li className="Button">
+            <Link to="/exercises" className="button-link">Exercises</Link>
           </li>
-          <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          <li className="Button">
+            <Link to="/meals" className="button-link">Meals</Link>
           </li>
-          <li className="navbar-item">
-          <Link to="/user" className="nav-link">Create User</Link>
-          </li>
-        </ul>
         </div>
       </nav>
+    </>
     );
   }
 }
