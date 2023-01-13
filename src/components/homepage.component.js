29140bcb6348446ccea5583cfc9eff08a6c1afea
unknown <goh.yenther@gmail.com>
2023-01-13 15:53:33 +0800

    homepage component

---
+++ src/components/homepage.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Welcome to your health tracker!</h3>
+      </div>
+    )
+  }
+}
