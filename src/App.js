321112eb72324585ed0d58928b65124ba421c8cd
unknown <goh.yenther@gmail.com>
2023-01-13 15:11:43 +0800

    add more links to app

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/exercises-list.component";
+import MealsList from "./components/meals-list.component";
 import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
+import EditMeal from "./components/edit-meal.component";
 import CreateUser from "./components/create-user.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateMeal from "./components/create-meal.component";
+
 
 function App() {
   return (
@@ -14,9 +18,12 @@ function App() {
       <div className="container">
       <Navbar />
       <br/>
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
+      <Route path="/exercises" exact component={ExercisesList} />
+      <Route path="/meals" exact component={MealsList} />
+      <Route path="/exerciseEdit/:id" component={EditExercise} />
+      <Route path="/mealEdit/:id" component={EditMeal} />
+      <Route path="/createExercise" component={CreateExercise} />
+      <Route path="/createMeal" component={CreateMeal} />
       <Route path="/user" component={CreateUser} />
       </div>
     </Router>
